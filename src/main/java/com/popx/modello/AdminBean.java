b59d7034b0462f5c01525193cef46a5dbaac4d25
Daniele Pio Scaparra <162312256+dscap02@users.noreply.github.com>
2025-11-30 16:40:10 +0100

    docs: added JML specs for GestoreOrdineBean and AdminBean classes Added invariants, pre/post conditions for OpenJML verification.

--- src/main/java/com/popx/modello/AdminBean.java
+++ src/main/java/com/popx/modello/AdminBean.java
@@ -1,11 +1,50 @@
 package com.popx.modello;
 
-
-
+/*@
+  @ invariant this.getRole() == null
+  @        || this.getRole().equals("Admin");
+@*/
 public class AdminBean extends UserBean {
+
+    /*@
+     @ ensures this.getUsername() == null
+     @      && this.getEmail() == null
+     @      && this.getPassword() == null
+     @      && this.getRole() == null;
+
+     */
 
     public AdminBean() {}
 
+    /*@
+      @ also
+      @ requires username != null && !username.isEmpty();
+
+      @ requires email != null;
+      @ requires !email.isEmpty();
+      @ requires email.contains("@");
+      @ requires email.indexOf('@') > 0;
+      @ requires email.substring(email.indexOf('@') + 1).contains(".");
+      @ requires email.indexOf('.') > email.indexOf('@') + 1;
+      @ requires email.lastIndexOf('.') < email.length() - 1;
+
+      @ requires password != null;
+      @ requires password.length() >= 8 && password.length() <= 16;
+      @ requires (\exists int i; 0 <= i && i < password.length();
+                    Character.isLowerCase(password.charAt(i)));
+      @ requires (\exists int i; 0 <= i && i < password.length();
+                    Character.isUpperCase(password.charAt(i)));
+      @ requires (\exists int i; 0 <= i && i < password.length();
+                    Character.isDigit(password.charAt(i)));
+
+      @ requires role != null;
+      @ requires role.equals("Admin");
+
+      @ ensures this.getUsername().equals(username)
+      @      && this.getEmail().equals(email)
+      @      && this.getPassword().equals(password)
+      @      && this.getRole().equals("Admin");
+    @*/
     public AdminBean(String username, String email, String password, String role) {
         super(username, password, email, role);
     }
--- src/main/java/com/popx/modello/GestoreOrdineBean.java
+++ src/main/java/com/popx/modello/GestoreOrdineBean.java
@@ -1,10 +1,53 @@
 package com.popx.modello;
 
+/*@
+  @ invariant this.getRole() == null
+  @        || this.getRole().equals("Gestore");
+@*/
+
 
 public class GestoreOrdineBean extends UserBean {
+
+    /*@
+     @ ensures this.getUsername() == null
+     @      && this.getEmail() == null
+     @      && this.getPassword() == null
+     @      && this.getRole() == null;
+   @*/
     public GestoreOrdineBean() {}
 
+
+    /*@
+      @ also
+      @ requires username != null && !username.isEmpty();
+
+      @ requires email != null;
+      @ requires !email.isEmpty();
+      @ requires email.contains("@");
+      @ requires email.indexOf('@') > 0;
+      @ requires email.substring(email.indexOf('@') + 1).contains(".");
+      @ requires email.indexOf('.') > email.indexOf('@') + 1;
+      @ requires email.lastIndexOf('.') < email.length() - 1;
+
+      @ requires password != null;
+      @ requires password.length() >= 8 && password.length() <= 16;
+      @ requires (\exists int i; 0 <= i && i < password.length();
+                    Character.isLowerCase(password.charAt(i)));
+      @ requires (\exists int i; 0 <= i && i < password.length();
+                    Character.isUpperCase(password.charAt(i)));
+      @ requires (\exists int i; 0 <= i && i < password.length();
+                    Character.isDigit(password.charAt(i)));
+
+      @ requires role != null;
+      @ requires role.equals("Gestore");
+
+      @ ensures this.getUsername().equals(username)
+      @      && this.getEmail().equals(email)
+      @      && this.getPassword().equals(password)
+      @      && this.getRole().equals("Gestore");
+    @*/
     public GestoreOrdineBean(String username, String email, String password, String role) {
         super(username, email, password, role);
     }
+
 }
